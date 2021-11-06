853f3b710b40c0b9aac6acf75b13d9444195dad7
EunoChoi <64246481+EunoChoi@users.noreply.github.com>
2021-11-07 02:16:03 +0900

    add transition

--- js/responsive-header.js
+++ js/responsive-header.js
@@ -1,10 +1,12 @@
 const barsButton = document.querySelector(".header-area__bars");
 const headerMenu = document.querySelector(".header-area__menu");
 const headerIcons = document.querySelector(".header-area__icons");
+const headerArea = document.querySelector(".header-area");
 
 function headerMenuToggle() {
     headerMenu.classList.toggle("hidden");
     headerIcons.classList.toggle("hidden");
+    headerArea.classList.toggle("mini");
 }
 
 barsButton.addEventListener("click", headerMenuToggle);
